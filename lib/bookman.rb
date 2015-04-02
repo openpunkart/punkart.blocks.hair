d77f2963a6a91c711627f523366eb3d731ef7bf9
Gerald Bauer <gerald.bauer@gmail.com>
2015-04-02 10:27:08 +0200

    everything is new

---
+++ lib/bookman.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+
+####################
+# our own code
+
+require 'bookman/version'  # let it always go first
+
+
+
+# say hello
+puts Bookman.banner   if defined?($RUBYLIBS_DEBUG) && $RUBYLIBS_DEBUG
+
